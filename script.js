ab462cda1bfe18ff6c1986ebfd153f6d3fd33bdd
anash5119 <anash5119@gmail.com>
2023-06-19 10:42:43 -0700

    Add comments for clarity

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let compWins = 0;
 
 function getComputerChoice(){
     const arrChoice = ["Rock", "Paper", "Scissors"];
-    return arrChoice[Math.floor(((Math.random())*3))];
+    return arrChoice[Math.floor(((Math.random())*3))]; //pick an array indice between 0 and 2
 }
 
 function play(playerSelection, computerSelection){
@@ -14,29 +14,30 @@ function play(playerSelection, computerSelection){
         || playerChoice == "paper" && computerSelection == "Rock"
         || playerChoice == "scissors" && computerSelection == "Paper"){
         
-        playerWins++;
+        playerWins++; //add to the player wins counter
         return "You win! " + playerSelection + " beats " + computerSelection;
     }
     else if (playerChoice == "rock" && computerSelection == "Paper"
             || playerChoice == "paper" && computerSelection == "Scissors"
             || playerChoice == "scissors" && computerSelection == "Rock"){
             
-        compWins++;        
+        compWins++; //add to the computer wins counter
         return "You Lose! " + computerSelection + " beats " + playerSelection;
     }
     else {
         return "You tie!";
     }
-
 }
 
 function game(){
+    //5 round match
     for(let index = 0; index < 5; index++){
         let player = prompt("Rock, Paper, or Scissors?", "Rock");
         let computer = getComputerChoice();
         console.log(play(player, computer))
     }
     
+    //decide who won the match
     if(playerWins > compWins){
         console.log("You win the 5 round match!");
     }
