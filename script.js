faa4841397624582a682d40459a260b928cd116b
anash5119 <anash5119@gmail.com>
2023-06-21 17:30:32 -0700

    Add UI with usable buttons

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 let playerWins = 0;
 let compWins = 0;
 
+const buttons = document.querySelectorAll('.playOption');
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        game(button.id);
+    });
+});
+
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener("click", () => {
+    const playerSpan = document.querySelector(".playerSpan")
+    playerSpan.textContent = 0;
+    const compSpan = document.querySelector(".compSpan")
+    compSpan.textContent = 0;
+    playerWins = 0;
+    compWins = 0;
+});
+
+
 
 function getComputerChoice(){
     const arrChoice = ["Rock", "Paper", "Scissors"];
@@ -15,6 +33,8 @@ function play(playerSelection, computerSelection){
         || playerChoice == "scissors" && computerSelection == "Paper"){
         
         playerWins++; //add to the player wins counter
+        const playerSpan = document.querySelector(".playerSpan")
+        playerSpan.textContent = playerWins;
         return "You win! " + playerSelection + " beats " + computerSelection;
     }
     else if (playerChoice == "rock" && computerSelection == "Paper"
@@ -22,6 +42,8 @@ function play(playerSelection, computerSelection){
             || playerChoice == "scissors" && computerSelection == "Rock"){
             
         compWins++; //add to the computer wins counter
+        const compSpan = document.querySelector(".compSpan")
+        compSpan.textContent = compWins;
         return "You Lose! " + computerSelection + " beats " + playerSelection;
     }
     else {
@@ -29,22 +51,28 @@ function play(playerSelection, computerSelection){
     }
 }
 
-function game(){
+function game(player){
     //5 round match
-    for(let index = 0; index < 5; index++){
-        let player = prompt("Rock, Paper, or Scissors?", "Rock");
-        let computer = getComputerChoice();
-        console.log(play(player, computer))
-    }
+    let computer = getComputerChoice();
+    console.log(play(player, computer))
     
     //decide who won the match
-    if(playerWins > compWins){
-        console.log("You win the 5 round match!");
+    if(playerWins === 5 || compWins === 5){
+        if(playerWins > compWins){
+            console.log("You win the 5 round match!");
+            winAnimation();
+        }
+        else if(compWins > playerWins){
+            console.log("You lose the 5 round match!");
+            loseAnimation();
+        }
     }
-    else if(compWins > playerWins){
-        console.log("You lose the 5 round match!");
-    }
-    else{
-        console.log("A tie!");
-    }
+}
+
+function winAnimation(){
+    document.getElementByClassName("winOverlay").style.display="block";
+}
+
+function loseAnimation(){
+
 }
